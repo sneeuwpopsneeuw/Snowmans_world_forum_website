72bb4f804a27456378b701908d88b528d3680392
sneeuwpopsneeuw <sneeuwpopsneeuw@live.nl>
2015-05-18 11:23:40 +0200

    update 0.1

--- index.php
+++ index.php
@@ -1,7 +1,34 @@
+<!DOCTYPE html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <link rel="stylesheet" type="text/css" href="blended_layout.css">
+    <title>Page Title</title>
+    <meta name="description" content="Write some words to describe your html page">
+</head>
+<body>
+<div class="blended_grid">
+    <div class="topBanner">
+    </div>
+    <div class="middleBanner">
+    </div>
+    <div class="leftBanner">
+    </div>
+    <div class="centerBanner">
+    </div>
+    <div class="rightBanner">
+    </div>
+    <div class="bottomBanner">
+    </div>
+</div>
+
 <?php
 /**
  * Created by PhpStorm.
  * User: Sneeuwpopsneeuw
  * Date: 18-5-2015
  * Time: 10:32
- */ 
+ */
+?>
+
+</body>
+</html>
