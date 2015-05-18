dab2a9638e1482fa32fde404067bd673d9643f88
sneeuwpopsneeuw <sneeuwpopsneeuw@live.nl>
2015-05-18 11:31:27 +0200

    update 0.2

--- index.php
+++ index.php
@@ -1,23 +1,29 @@
 <!DOCTYPE html>
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
-    <link rel="stylesheet" type="text/css" href="blended_layout.css">
-    <title>Page Title</title>
-    <meta name="description" content="Write some words to describe your html page">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <title>SnowmansWorld</title>
+    <meta name="description" content="Forum Webite">
 </head>
 <body>
 <div class="blended_grid">
     <div class="topBanner">
+        topBanner
     </div>
     <div class="middleBanner">
+        middleBanner
     </div>
     <div class="leftBanner">
+        leftBanner
     </div>
     <div class="centerBanner">
+        centerBanner
     </div>
     <div class="rightBanner">
+        rightBanner
     </div>
     <div class="bottomBanner">
+        bottomBanner
     </div>
 </div>
 
