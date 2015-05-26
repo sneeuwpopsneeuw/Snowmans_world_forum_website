b6f02333d9a7480ed916550f372a59200d2b23ec
sneeuwpopsneeuw <sneeuwpopsneeuw@live.nl>
2015-05-26 10:46:01 +0200

    update 4.0

--- forum.php
+++ forum.php
@@ -1,24 +1,7 @@
 <?php include 'templates/header.php'; ?>
 
-<div class="blended_grid">
-    <div class="topBanner">
-        topBanner
-    </div>
-    <div class="middleBanner">
-        middleBanner
-    </div>
-    <div class="leftBanner">
-        leftBanner
-    </div>
-    <div class="centerBanner">
-        centerBanner
-    </div>
-    <div class="rightBanner">
-        rightBanner
-    </div>
-    <div class="bottomBanner">
-        bottomBanner
-    </div>
+<div class="container">
+    hoi
 </div>
 
 <?php include 'templates/footer.php'; ?>
